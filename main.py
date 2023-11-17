b0e10034e314a4b507e6e71db809bad1f8cbfb55
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-11-17 13:51:37 +0300

    Simplify get_form endpoint.

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+from typing import Any
+
 import uvicorn
 from fastapi import FastAPI, Request
 
 from database import prepare_db, find_matching_template
-from validators import is_field_valid, infer_field_types
+from services import get_field_types
 
 app = FastAPI()
 
 
-@app.get("/get_form")
-async def get_form(request: Request):
-    params = dict(request.query_params)
-    data_types = infer_field_types(params)
-    for field_type, value in zip(data_types.values(), params.values()):
-        if not is_field_valid(value, field_type):
-            return {field_type: "Validation error"}
-    if matching_template := find_matching_template(params):
+@app.post("/get_form")
+async def get_form(request: Request) -> dict[str, Any]:
+    data_types = get_field_types(dict(request.query_params))
+    if matching_template := find_matching_template(data_types):
         return {"form_name": matching_template}
     else:
         return data_types
